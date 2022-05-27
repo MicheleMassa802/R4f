6a29226da9b32feaf0910718886a3b51d57b4af6
Michele Massa <michele.massa@mail.utoronto.ca>
2022-05-27 13:53:54 -0400

    Email sending helper and testing of email sending in main completed

--- R4f_project/src/main/java/Main.java
+++ R4f_project/src/main/java/Main.java
@@ -1,8 +1,13 @@
 package main.java;
 
+import main.java.Helpers.SendEmail;
+
 public class Main {
     public static void main(String[] args) {
         // main code here ...
-        System.out.println("Hello world");
+        System.out.println("Email sending testing in progress");
+        SendEmail sender = new SendEmail("admin", "massamichele802@gmail.com", "bd69420");
+        sender.executeSend();
+        System.out.println("End of test");
     }
 }

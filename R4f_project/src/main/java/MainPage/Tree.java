d39b217e82f1e27a0427f83478501ed14b5d2f1f
Michele Massa <michele.massa@mail.utoronto.ca>
2022-07-06 18:00:42 -0400

    setup tree manager functions and hardcoded the tree structure for R4F

--- R4f_project/src/main/java/MainPage/Tree.java
+++ R4f_project/src/main/java/MainPage/Tree.java
@@ -2,17 +2,24 @@ package main.java.MainPage;
 
 import java.util.ArrayList;
 
-public class Tree {
+public class Tree{
+
+    public enum TreeState {
+        WELCOME, HOME, ACCOUNT, CALENDAR, ABOUT;
+    }
+
     // class for structuring the tree for traversing the different pages to be displayed
     private Tree parent;
-    private String nodeName;  // Invariant: this String should be unique within the tree nodes
+    private TreeState stateName;  // Invariant: this String should be unique within the tree nodes
+    private String currPageName;
     private ArrayList<Tree> subtrees;
-    private ITreeNode toExecute;
+    
 
     /** Usage:  
      * moving through the tree consists of creating the tree with the correct structure, and then keeping  a 
      * root variable managing the current stage of the program by containing the current Tree node being
      * executed
+     * Based on the state, the main function managing the tree will be given certain permissions and attributes to use
     */
 
     /**
@@ -23,14 +30,14 @@ public class Tree {
      * Ideally, you'd have a variable to describe the current place in the tree were you are (such as a root
      * variable), and then have them connected through their parent and subtrees attributes. 
      * 
-     * @param name      the name describing the code to be executed stored in this tree node, this should be
+     * @param parent    the tree node representing the previous (parent) state of the program
+     * @param state     the name describing the page being executed stored in this tree node, this should be
      *                  unique within the tree
-     * @param toExecute the page object with its execute method which is called by the Tree's execute method
+     * @param pageName  the string name ending in .html containning the reference name of the page to load
      */
-    public Tree(Tree parent, String name, ITreeNode toExecute){
+    public Tree(Tree parent, TreeState state, String pageName){
         this.parent = parent;  // for the root of a tree set to NULL
-        this.nodeName = name;  // should not be null
-        this.toExecute = toExecute;  // should not be null and be from a class implementing ITreeNode
+        this.stateName = state;  // should not be null
         this.subtrees = new ArrayList<Tree>();  // tree has no subtrees by default
     }
 
@@ -40,7 +47,7 @@ public class Tree {
      */
     public boolean isEmpty(){
 
-        if (this.nodeName == null || this.toExecute == null){
+        if (this.stateName == null || this.currPageName == ""){
             return true;
         }
         return false;
@@ -67,21 +74,15 @@ public class Tree {
 
 
     /**
-     * execute the code to run the page for the current node
-     */
-    public void execute(){
-        this.toExecute.execute();  // call the execution method of the object implementing the ITreeNode interface
-    }
-
-
-    /**
      * add a new child to the tree  (this is used for the buildup of the standard constant tree)
-     * @param nodeName node String name for the subtree being added (linked)
-     * @param toExecute ITreeNode object representing the page of the GUI to be run when in this state of the tree
+     * @param stateName node state name for the subtree being added (linked)
+     * @param pageName  the string name ending in .html containning the reference name of the page to load
+     * @return          the child Tree that just got created
      */
-    public void addSub(String nodeName, ITreeNode toExecute){
-        Tree subTree = new Tree(this, nodeName, toExecute);  // create new tree instance
+    public Tree addSub(TreeState stateName, String pageName){
+        Tree subTree = new Tree(this, stateName, pageName);  // create new tree instance
         this.subtrees.add(subTree);  // link current tree and subtree
+        return subTree;
     }
 
 
@@ -89,10 +90,10 @@ public class Tree {
      * remove a child from the list of child subtrees
      * @param subToRemove the specific object to remove from the calling tree's subtrees attribute
      */
-    public void removeSub(String subNameToRemove){
+    public void removeSub(TreeState subState){
 
         for (Tree subTree : this.subtrees){
-            if (subTree.nodeName == subNameToRemove){
+            if (subTree.stateName == subState){
                 this.subtrees.remove(subTree);  // remove the subtree matching in nodeName
             }
         }
--- R4f_project/src/main/java/MainPage/TreeManager.java
+++ R4f_project/src/main/java/MainPage/TreeManager.java
@@ -1,5 +1,113 @@
 package main.java.MainPage;
 
+import main.java.Controllers.BirthdayController;
+import main.java.Controllers.CalendarController;
+import main.java.Controllers.UserController;
+import main.java.MainPage.Tree;
+import main.java.MainPage.Tree.TreeState;
+
 public class TreeManager {
-    // class operating the jumps between different nodes in the tree
+
+    // Page name constants
+    static final String WELCOME = "welcome.html";
+    static final String MAIN = "index.html";
+    static final String ACCOUNT = "account.html";
+    static final String CALENDAR = "calendar.html";
+    static final String ABOUT = "about.html";
+    static final String ABOUT_NO_NAV = "aboutNoNav.html";
+
+
+    private Tree currentNode;
+    private BirthdayController bdController;
+    private CalendarController calController;
+    private UserController userController;
+
+    /** Constructor which starts off the controllers to be used throughout the lifespan of the session */
+    public TreeManager(){
+        // start off the default build of the tree
+        this.currentNode = buildR4fDefault();  // starts off at the welcome parent
+        // build the controllers
+        this.bdController = new BirthdayController();
+        this.userController = new UserController();
+    }
+
+    /**
+     * setup the calendar controller attribute once logged in
+     * @param userId    the id of the user that is logged in to observe/create their calendar
+     */
+    public void addCalendar(String userId){
+        this.calController = new CalendarController(userId);
+    }
+
+    /**
+     * Helper static method which works regardless of whether or not a Tree instance exists to create a R4F tree
+     * @return a tree suited to the layout and structure of the R4F website pages
+     */
+    public static Tree buildR4fDefault(){
+        // stage 0, start
+        Tree parent = new Tree(null, TreeState.WELCOME, WELCOME);
+        // 1st stage (main and first about us)
+        Tree mainPage = parent.addSub(TreeState.HOME, MAIN);
+        parent.addSub(TreeState.ABOUT, ABOUT_NO_NAV);  // aboutNoNav
+        // 2nd stage (after mainPage: calendar, account, about us)
+        mainPage.addSub(TreeState.CALENDAR, CALENDAR);  // calendar
+        mainPage.addSub(TreeState.ACCOUNT, ACCOUNT);  // account
+        mainPage.addSub(TreeState.ABOUT, ABOUT);  // about
+        // to move onto the nav bar elements, the transition would be to move onto the main page 1st stage, then onto the 2nd stage child
+        return parent;
+    }
+
+
+    // PAGE SPECIFIC FUNCTIONS  
+
+    // welcome page
+
+    public void logUserIn(){
+        // if state is welcome
+
+        // move tree to first stage mainPage
+    }
+
+    public void registerUser(){
+        // if state is welcome
+
+        // move tree to 0th stage welcome page (so user logs in), so no change in tree state
+    }
+
+    public void moveToAboutUsNoNav(){
+        // if state is welcome
+
+        // move tree position to first stage about us
+    }
+
+
+    // index page
+
+    public void showCalendar(){
+        // if state is home
+
+        // move tree position to second stage calendar
+
+    } 
+
+    public void showAccount(){
+        // if state is home
+
+        // move tree position to second stage account
+
+    }
+
+    public void moveToAboutUs(){
+        // if state is home
+
+        // move tree position to second stage about us
+
+    } 
+
+
+    public static void main(String[] args) {
+
+    }
+
+    
 }

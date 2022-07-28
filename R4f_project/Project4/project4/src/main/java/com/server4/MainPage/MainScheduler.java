9707eabeb4b65298330c4e5761ad76acbd7bbc13
Michele Massa <michele.massa@mail.utoronto.ca>
2022-07-28 13:33:03 -0400

    reset email sending time to 7:00am

--- R4f_project/Project4/project4/src/main/java/com/server4/MainPage/MainScheduler.java
+++ R4f_project/Project4/project4/src/main/java/com/server4/MainPage/MainScheduler.java
@@ -6,9 +6,9 @@ public class MainScheduler {
     
     public static void main(String[] args) {
         
-        // these two ints describe the time at which the emails will be sent
-        final int HOUR = 13;  // 0 - 23
-        final int MINUTE = 30;  // 0 - 59
+        // these two ints describe the time at which the emails will be sent (7:00 am by default)
+        final int HOUR = 7;  // 0 - 23
+        final int MINUTE = 0;  // 0 - 59
 
         // run email scheduler
         Notifier.ScheduleNotification(HOUR, MINUTE);;  // schedule the notification emails to be sent to at the input hour and minutes
